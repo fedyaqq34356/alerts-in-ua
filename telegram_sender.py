93d1a1f8d46c133d29edda86784cda59e5b1b1f6
fedya <fedya.3.4.6.2.7@gmail.com>
2026-01-08 16:39:33 +0200

    edit telegram_sender.py

--- telegram_sender.py
+++ telegram_sender.py
@@ -1,7 +1,12 @@
 from aiogram import Bot
+from aiogram.enums import ParseMode
 from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHANNEL_ID
 
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
 
 async def send_message_to_channel(text: str):
-    await bot.send_message(chat_id=TELEGRAM_CHANNEL_ID, text=text)
+    await bot.send_message(
+        chat_id=TELEGRAM_CHANNEL_ID, 
+        text=text,
+        parse_mode=ParseMode.HTML
+    )
